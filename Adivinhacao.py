26e538df055e11ae1cf4f32f19d87c9b06e5a265
juliadouradob <82419523+juliadouradob@users.noreply.github.com>
2021-04-20 15:39:26 -0300

    :sparkles: Improve code performance

--- Adivinhacao.py
+++ Adivinhacao.py
@@ -1,46 +1,51 @@
 import random
 
-print("********************************")
-print("Bem Vindo ao jogo de Adivinhacao")
-print("********************************")
 
-numero = random.randrange(1, 101)
-tentativas = 0
-pontos = 1000
+def jogar():
+    print("********************************")
+    print("Bem Vindo ao jogo de Adivinhacao")
+    print("********************************")
 
-print("Qual o nível de dificuldade?")
-print("(1) Fácil \n(2) Médio \n(3) Difícil\n")
+    numero = random.randrange(1, 101)
+    tentativas = 0
+    pontos = 1000
 
-nivel = int(input("Nível: "))
+    print("Qual o nível de dificuldade?")
+    print("(1) Fácil \n(2) Médio \n(3) Difícil\n")
 
-if (nivel == 1):
-    tentativas = 20
-elif (nivel == 2):
-    tentativas = 10
-else:
-    tentativas = 5
+    nivel = int(input("Nível: "))
 
-for rodada in range(1, tentativas + 1):
-    print("------------------")
-    print("  RODADA: {} de {}".format(rodada, tentativas))
-    print("------------------")
-
-    numero_usuario = int(input("Digite um número entre 1 e 100: "))
-
-    if (numero_usuario < 1 or numero_usuario > 100):
-        print("Número inválido! Tente novamente")
-        continue
-
-    if (numero_usuario == numero):
-        print("Voce acertou!")
-        break
+    if (nivel == 1):
+        tentativas = 20
+    elif (nivel == 2):
+        tentativas = 10
     else:
-        if (numero_usuario > numero):
-            print("Voce chutou maior que número secreto!")
-        elif (numero_usuario < numero):
-            print("Voce chutou menor que o número secreto!")
+        tentativas = 5
 
-        pontos = pontos - (abs(numero - numero_usuario))
+    for rodada in range(1, tentativas + 1):
+        print("------------------")
+        print("  RODADA: {} de {}".format(rodada, tentativas))
+        print("------------------")
 
-print("Fim do jogo!")
-print("Total de pontos: " + str(pontos))
+        numero_usuario = int(input("Digite um número entre 1 e 100: "))
+
+        if (numero_usuario < 1 or numero_usuario > 100):
+            print("Número inválido! Tente novamente")
+            continue
+
+        if (numero_usuario == numero):
+            print("Voce acertou!")
+            break
+        else:
+            if (numero_usuario > numero):
+                print("Voce chutou maior que número secreto!")
+            elif (numero_usuario < numero):
+                print("Voce chutou menor que o número secreto!")
+
+            pontos = pontos - (abs(numero - numero_usuario))
+
+    print("Fim do jogo!")
+    print("Total de pontos: " + str(pontos))
+
+if (__name__ == "__main__"):
+    jogar()
